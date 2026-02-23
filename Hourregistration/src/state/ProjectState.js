e74f31aad052eb855effa782f889e25cb2f0992d
ltielbeke <l.tielbeke@wewo.nl>
2026-02-23 20:09:09 +0100

    Add update functionality for projects and improve project management UI.
    - Added `EditProject` component for editing project details.
    - Introduced `updateProject` function in `Index.vue` and integrated it into the UI.
    - Updated `ProjectState.js` to include `UpdateProject` method.
    - Replaced environment API URL with local development URL in `.env`.

--- Hourregistration/src/state/ProjectState.js
+++ Hourregistration/src/state/ProjectState.js
@@ -72,6 +72,7 @@ export const useProjectState =  defineStore('ProjectStore', {
                 }
             }
         },
+        
         async DeleteProject(id) {
             try {
                 return await fetchWithBaseUrl("api/project/" + id, {
@@ -86,6 +87,33 @@ export const useProjectState =  defineStore('ProjectStore', {
                         status: response.status
                     }
                 });
+            } catch (error) {
+                return {
+                    success: false,
+                    data: [],
+                    code: error.code
+                }
+            }
+        },
+        
+        async UpdateProject(project) {
+            try {
+                return await fetchWithBaseUrl("api/project/" + project.id, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        Name: project.Name,
+                        Description: project.Description
+                    })
+                }).then((response) => {
+                    return {
+                        success: true,
+                        data:  response.json(),
+                        status: response.status
+                    }
+                })
             } catch (error) {
                 return {
                     success: false,
